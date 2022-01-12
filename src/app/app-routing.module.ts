5f60a7b4331d35af883b4e854e12add92579eff9
Cecilia Voloski <ceciliavoloski@gmail.com>
2022-01-12 18:23:55 -0300

    Gerando rotas para navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent} /* na URL o www.com.br/ após esse barra, caso não seja encontrado, indicará esse erro*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoryComponent } from './category/category.component';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material.module';
 import { TesteComponent } from './teste/teste.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      CategoryComponent,
       HeaderComponent,
       TesteComponent,
-      FooterComponent
+      FooterComponent,
+      HomeComponent,
+      CategoryComponent
    ],
   imports: [
     BrowserModule,
